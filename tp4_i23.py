eace405716aa89982ceb0e91637d81db4fb520a9
Cobrette <78154581+Cobrette@users.noreply.github.com>
2021-04-16 10:14:50 +0200

    Add files via upload

--- tp4_i23.py
+++ tp4_i23.py
@@ -15,9 +15,55 @@ def EstPermutation(s):
 
 #question 2:
 def Inverser(s):
+    l=[0]*lens(s)
+    for i in range(0,len(s)):
+        l[s[i]]=1
+    return tuple(l)
+   
+#question 3:
+def composer(s,t):
+    p=[0]*len(s)
+    if len(s)!=len(t):
+        return('pas la meme longueur')
+    elif not (EstPermutation(s) and EstPermutations(t)):
+        return ('ce ne sont pas des permutations')
+    else:
+        i=0
+        for e in t:
+            p[s[i]]=e
+            i+=1
+    return Ecrire(s)
+
+#question 4:
+def orbite(k,s):
+    k=k-1
+    resultat=(k,)
+    debut=k
+    if not EstPermutation(s):
+        print('pas de permutation')
+    else:
+        while s[k] != debut :
+            resultat += (s[k],)
+            k=s[k]
+    return resultat
+
+
+#question 5:
+def Signature(s):
+    pass    
+
+#question 6:
+def Cycles(s):
+    pass
+    
+#question 7:
+def Transposition(s):
+    pass
 
 #main
 perm = lire()
 Ecrire(perm)
 permu=EstPermutation(perm)
 print(permu)
+print(composer(a,c))
+print (Ecrire(orbite(1,lire())))
